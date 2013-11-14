b542ac43d1594a1c6aeaf3ed22c94ebef1694146
hiyoko9191 <hiyoko9191@gmail.com>
2013-11-14 15:30:27 +0900

    contact page

--- client/get_together/src/com/example/get_together/ContactActivity.java
+++ client/get_together/src/com/example/get_together/ContactActivity.java
@@ -1,16 +1,99 @@
 package com.example.get_together;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.Window;
+	import java.util.ArrayList;
+	import java.util.HashMap;
+	import java.util.List;
+	import java.util.Map;
 
-public class ContactActivity extends Activity {
+	import android.os.Bundle;
+	import android.app.Activity;
+	import android.view.Menu;
+	import android.widget.ArrayAdapter;
+	import android.widget.ListView;
+	import android.widget.SimpleAdapter;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.activity_contact);
+	public class ContactActivity extends Activity {
+
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.activity_contact);
+
+			//get contact  list
+			ListView lv = (ListView) findViewById(R.id.contactList);
+			SimpleAdapter adapter = new SimpleAdapter(this,getContact(),R.layout.contact_list,
+					new String[]{"friendHead","friendName"},
+					new int[]{R.id.friendHead,R.id.friendName});
+			lv.setAdapter(adapter);
+				
+				
+		}
+		
+		//get contact  list
+		private List<Map<String, Object>> getContact() {
+			
+			List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("friendHead", R.drawable.header01);
+			map.put("friendName", "Aさん");
+			list.add(map);
+
+			map = new HashMap<String, Object>();
+			map.put("friendHead", R.drawable.header02);
+			map.put("friendName", "Bさん");
+			list.add(map);
+
+			map = new HashMap<String, Object>();
+			map.put("friendHead", R.drawable.header03);
+			map.put("friendName", "Cさん");
+			list.add(map);
+			
+			map = new HashMap<String, Object>();
+			map.put("friendHead", R.drawable.header01);
+			map.put("friendName", "Aさん");
+			list.add(map);
+
+			map = new HashMap<String, Object>();
+			map.put("friendHead", R.drawable.header02);
+			map.put("friendName", "Bさん");
+			list.add(map);
+
+			map = new HashMap<String, Object>();
+			map.put("friendHead", R.drawable.header03);
+			map.put("friendName", "Cさん");
+			list.add(map);
+			
+			map = new HashMap<String, Object>();
+			map.put("friendHead", R.drawable.header01);
+			map.put("friendName", "Aさん");
+			list.add(map);
+
+			map = new HashMap<String, Object>();
+			map.put("friendHead", R.drawable.header02);
+			map.put("friendName", "Bさん");
+			list.add(map);
+
+			map = new HashMap<String, Object>();
+			map.put("friendHead", R.drawable.header03);
+			map.put("friendName", "Cさん");
+			list.add(map);
+			
+			map = new HashMap<String, Object>();
+			map.put("friendHead", R.drawable.header01);
+			map.put("friendName", "Aさん");
+			list.add(map);
+
+			map = new HashMap<String, Object>();
+			map.put("friendHead", R.drawable.header02);
+			map.put("friendName", "Bさん");
+			list.add(map);
+
+			map = new HashMap<String, Object>();
+			map.put("friendHead", R.drawable.header03);
+			map.put("friendName", "Cさん");
+			list.add(map);
+			
+			return list;
+		}
 	}
-}
